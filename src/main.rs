9e68be448e8f8746347da302fd7b36acd588a062
vickyone0 <vigneshs0.618@gmail.com>
2025-08-07 10:33:26 -0400

    select in tokio

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     let start_time = Instant::now();
     tokio::spawn(heartbeat(0));
-    let (status_1,status_2) = tokio::join!(get_status("https://google.com"),get_status("https://google.com"));
-    println!("status 1: {}",status_1.unwrap());
-    println!("status 2: {}",status_2.unwrap());
+    //let (status_1,status_2) = tokio::join!(get_status("https://google.com"),get_status("https://google.com"));
+
+    tokio::select! (
+        stat = get_status("https://google.com")=> println!("status : {:?}",stat),
+        stat = get_status("https://google.com")=> println!("status : {:?}",stat),
+    );
+    // println!("status 1: {}",status_1.unwrap());
+    // println!("status 2: {}",status_2.unwrap());
 
 
     println!("Overall execution time: {}ms",start_time.elapsed().as_millis());
