f83e658684dd52e812098f740a1a44feb8f60216
vickyone0 <vigneshs0.618@gmail.com>
2025-08-07 11:58:36 -0400

    tokio:: mpsc for message trafer

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn heartbeat(mut num: u32) {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     let start_time = Instant::now();
-    tokio::spawn(heartbeat(0));
+    //tokio::spawn(heartbeat(0));
     //let (status_1,status_2) = tokio::join!(get_status("https://google.com"),get_status("https://google.com"));
 
     tokio::select! (
@@ -29,6 +29,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
     println!("Overall execution time: {}ms",start_time.elapsed().as_millis());
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(5);
+    tokio::spawn(send_messages(tx));
+    while let Some(message) = rx.recv().await {
+        println!("Received message: '{}'", message);
+    }
     Ok(())
 }
 
@@ -48,4 +53,23 @@ async fn get_status(url: &str) -> Result<StatusCode, Box<dyn std::error::Error>>
 
     println!("Took {} ma to featch url '{}'.",duration, url);
     Ok(status_code)
+}
+
+async fn receive_messages(mut rx: tokio::sync::mpsc::Receiver<String>){
+while let Some(message) = rx.recv().await {
+    println!("Received message: '{}'", message);
+}
+
+}
+
+
+async fn send_messages(tx: tokio::sync::mpsc::Sender<String>){
+    let mut message_nr = 1;
+
+    loop {
+        tx.send(format!("hello from async [{message_nr}]")).await.unwrap();
+
+        message_nr +=1;
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
 }
